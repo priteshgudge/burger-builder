44eb48af6b56b5b3ea4d0f35841ec1e410e76017
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-09 13:36:20 +0530

    Burger Ingredient with CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
