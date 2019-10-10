34b73cc61d0fd6464f025e27b047febe4a928af5
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-10 13:00:35 +0530

    Toolbar and responsive navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
+import Toolbar from './UI/Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, Sidebar, Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
