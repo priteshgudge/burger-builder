a1bf5fecb58bb60ec223554813a9b3fe574e76d1
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-11 14:13:14 +0530

    Checkout summary added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <BurgerBuilder />
+        <Checkout/>
       </Layout>
     </div>
   );
